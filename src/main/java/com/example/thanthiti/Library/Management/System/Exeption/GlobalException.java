2af1691757cedfec8a057a32379cb102b60e22cc
Thanthiti <145197437+Thanthiti@users.noreply.github.com>
2025-08-24 23:44:23 +0700

    feat: implement global exception handler for centralized error handling

---
+++ src/main/java/com/example/thanthiti/Library/Management/System/Exeption/GlobalException.java
@@ -0,0 +1,33 @@
+package com.example.thanthiti.Library.Management.System.Exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalException {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, List<String>> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.computeIfAbsent(error.getField(), key -> new ArrayList<>())
+                    .add(error.getDefaultMessage());
+        });
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation failed",
+                "Bad Requestes",
+                errors
+        );
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+}
