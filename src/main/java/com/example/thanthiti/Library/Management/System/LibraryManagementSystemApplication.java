2dbb703a1721a58ec6198a29924eae1af70ecc4d
Thanthiti <145197437+Thanthiti@users.noreply.github.com>
2025-08-25 17:26:32 +0700

    chore: add JWT dependencies and update application properties for JWT configuration

--- src/main/java/com/example/thanthiti/Library/Management/System/LibraryManagementSystemApplication.java
+++ src/main/java/com/example/thanthiti/Library/Management/System/LibraryManagementSystemApplication.java
@@ -1,13 +1,21 @@
 package com.example.thanthiti.Library.Management.System;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryManagementSystemApplication {
+    public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.configure()
+            .ignoreIfMissing()
+            .load();
+    System.setProperty("DB_URL", dotenv.get("DB_URL"));
+    System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+    System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+    System.setProperty("JWT_SECRET_BASE64", dotenv.get("JWT_SECRET_BASE64", "defaultDevSecret"));
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryManagementSystemApplication.class, args);
-	}
+    SpringApplication.run(LibraryManagementSystemApplication.class, args);
 
+    }
 }
