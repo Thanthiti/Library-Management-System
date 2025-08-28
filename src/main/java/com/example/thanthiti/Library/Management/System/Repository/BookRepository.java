79f66adb2bdb8c861446db44a3f36215185f94be
Thanthiti <145197437+Thanthiti@users.noreply.github.com>
2025-08-28 22:33:06 +0700

    refactor: add @Repository annotation to BookRepository and CategoryRepository

--- src/main/java/com/example/thanthiti/Library/Management/System/Repository/BookRepository.java
+++ src/main/java/com/example/thanthiti/Library/Management/System/Repository/BookRepository.java
@@ -2,9 +2,11 @@ package com.example.thanthiti.Library.Management.System.Repository;
 
 import com.example.thanthiti.Library.Management.System.Entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByTitle(String title);
 }
--- src/main/java/com/example/thanthiti/Library/Management/System/Repository/CategoryRepository.java
+++ src/main/java/com/example/thanthiti/Library/Management/System/Repository/CategoryRepository.java
@@ -2,9 +2,11 @@ package com.example.thanthiti.Library.Management.System.Repository;
 
 import com.example.thanthiti.Library.Management.System.Entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByNameIgnoreCase(String name);
     // ใช้สำหรับดึงข้อมูล (optional)
