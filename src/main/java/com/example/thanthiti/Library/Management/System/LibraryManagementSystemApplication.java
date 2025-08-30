17af7c5212372ccb9e4ff38aec53023fbe491f14
Thanthiti <145197437+Thanthiti@users.noreply.github.com>
2025-08-30 23:00:13 +0700

    change

--- src/main/java/com/example/thanthiti/Library/Management/System/LibraryManagementSystemApplication.java
+++ src/main/java/com/example/thanthiti/Library/Management/System/LibraryManagementSystemApplication.java
@@ -5,22 +5,38 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-LibraryManagementSystemApplication {
+public class LibraryManagementSystemApplication {
     public static void main(String[] args) {
-    Dotenv dotenv = Dotenv.configure()
-            .ignoreIfMissing()
-            .load();
-    System.setProperty("DB_URL", dotenv.get("DB_URL"));
-    System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-    System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-    System.setProperty("JWT_SECRET_BASE64", dotenv.get("JWT_SECRET_BASE64", "defaultDevSecret"));
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
 
-    System.setProperty("ADMIN_EMAIL", dotenv.get("ADMIN_EMAIL"));
-    System.setProperty("ADMIN_PASSWORD", dotenv.get("ADMIN_PASSWORD"));
-    System.setProperty("ADMIN_NAME", dotenv.get("ADMIN_NAME"));
+        String dbUrl = dotenv.get("DB_URL");
+        String dbUser = dotenv.get("DB_USER");
+        String dbPassword = dotenv.get("DB_PASS");
+        String jwtSecret = dotenv.get("JWT_SECRET_BASE64", "defaultDevSecret");
+        String adminEmail = dotenv.get("ADMIN_EMAIL");
+        String adminPassword = dotenv.get("ADMIN_PASSWORD");
+        String adminName = dotenv.get("ADMIN_NAME");
 
-    SpringApplication.run(LibraryManagementSystemApplication.class, args);
+        if(dbUrl == null || dbUser == null || dbPassword == null) {
+            throw new IllegalArgumentException("Missing DB environment variables!");
+        }
 
+        if(adminEmail == null || adminPassword == null || adminName == null) {
+            throw new IllegalArgumentException("Missing ADMIN environment variables!");
+        }
+
+        // Set System properties
+        System.setProperty("DB_URL", dbUrl);
+        System.setProperty("DB_USER", dbUser);
+        System.setProperty("DB_PASS", dbPassword);
+        System.setProperty("JWT_SECRET_BASE64", jwtSecret);
+        System.setProperty("ADMIN_EMAIL", adminEmail);
+        System.setProperty("ADMIN_PASSWORD", adminPassword);
+        System.setProperty("ADMIN_NAME", adminName);
+
+        // Run Spring Boot
+        SpringApplication.run(LibraryManagementSystemApplication.class, args);
     }
 }
